3ff181091205a21522283986347bac80754151dd
asbarros <anderson.barros0298@gmail.com>
2019-11-20 14:56:30 -0300

    Implementando a logica

--- calculator/src/components/Button.vue
+++ calculator/src/components/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="button"
-  @click="$emit('onCalcButtonClick', label)" :class="{double, triple, operation}">{{label}}</button>
+  @click="$emit('onClick', label)" :class="{double, triple, operation}">{{label}}</button>
 </template>
 
 <script>
